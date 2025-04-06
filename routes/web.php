91726cedaec070615c0c57fb6efbd9dc93957ae5
isabelaCosta95 <isabelaovc95@gmail.com>
2025-04-06 16:19:24 -0300

    CRUD Produtos

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FuncionarioController;
 use App\Http\Controllers\CidadeController;
 use App\Http\Controllers\VeiculoController;
+use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\CategoriaController;
+
 
 
 
@@ -17,3 +19,4 @@ Route::resource("funcionarios", FuncionarioController::class);
 Route::resource("cidades", CidadeController::class);
 Route::resource("veiculos", VeiculoController::class);
 Route::resource("categorias", CategoriaController::class);
+Route::resource("produtos", ProdutoController::class);
